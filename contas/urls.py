672eab924051ee9c5fbe645166895d76a1179593
HumbertoCP <joaohbmenezes@outlook.com>
2020-11-20 09:46:05 -0300

    versão 3 - 20/11/20

--- contas/urls.py
+++ contas/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'$^', views.paginaInicial),
-    url(r'^login/', views.login)
+    url(r'^login/', views.login),
+    url(r'^registro/', views.registro)
 ]
--- contas/views.py
+++ contas/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
 
 def paginaInicial(request):
-    return HttpResponse("Bem vindo a pagina do app de contas")
+    return render(request, "contas/home.html")
 def login(request):
-    return HttpResponse("Bem vindo a pagina de login")
+    return render(request, "contas/login.html")
+def registro(request):
+    return HttpResponse("PAGINA DE REGISTRO!")
